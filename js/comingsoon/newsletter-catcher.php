bb055bdd5d3f29c6112ca58e3c9d48be87ea215d
Isaac Cacho G <msn@swfideas.com>
2015-12-16 00:02:38 -0600

    fix mail

--- js/comingsoon/newsletter-catcher.php
+++ js/comingsoon/newsletter-catcher.php
@@ -2,9 +2,9 @@
 if($_POST){
     $name = 'GADAI web';
     $email = $_POST['form_email'];
-    $message = 'Solicita ser informada cuando el sitio web este listo.';
+    $message = 'La cuenta: '.$email.' solicita ser informada cuando el sitio web este listo.';
 
 //send email
-    mail("maindesign@gmail.com", "Avisar a - " .$email, $message);
+    mail("maindesign@gmail.com", "Avisar a - " . $email, $message);
 }
 ?>
